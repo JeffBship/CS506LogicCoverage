c9c78cacc5fef67e714c32371ef28b71d6266c97
Austin Barrett <austinbrrtt@gmail.com>
2018-04-15 23:19:37 -0400

    Updated initial comment in CalTest to include more information

--- test/cs506logiccoverage/CalTest.java
+++ test/cs506logiccoverage/CalTest.java
@@ -10,36 +10,46 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class CalTest {
-    
+
     /*
     Tests the cal method. Predicates investigated:
-        p1 = a
-            a is always active
-        p2 = b | (c & d)
-            p2 reachable when a = F
-        p3 = e
-            e is always active
-            p3 reachable when a = F
-            if e = T initially, loop will repeat until e = F
-    
+      p1 = a
+        p1 is always reachable
+        a is always active
+        cases: T, F
+      p2 = b || (c && d)
+        p2 reachable when a = F
+        p2b = !c || !d
+        p2c = !b && d
+        p2d = !b && c
+        cases: FFT, TFT, FTT, FTF
+        b active: FFT, TFT
+        c active: FFT, FTT
+        d active: FTF, FTT
+      p3 = e
+        p3 reachable when a = F
+        e is always active
+        if e = T initially, loop will repeat until e = F
+        cases: T, F
+
     Tests below tests how method counts days between Jan1, and Mar1, to ensure
     days in February are added correctly in leap years and non leap years.
-    */    
-    
+    */
+
     @Test
-    //Tests p1, a=T 
-    //Tests p2, bcd=TFT 
+    //Tests p1, a=T
+    //Tests p2, bcd=TFT
     //Tests p3, e=F
     public void test_P1aT_P2TFT_P3eF(){
         //(month1, day1, month2, day2, year)
         int result = Cal.cal(1, 1, 1, 3, 99);
         assertEquals(result, 2);
     }
-    
-    
-    
+
+
+
     @Test
-    //tests p1, a=T  
+    //tests p1, a=T
     //test p2, not reached
     //test p3, not reached
     public void test_P1aT(){
@@ -47,7 +57,7 @@ public class CalTest {
         int result = Cal.cal(1, 1, 1, 31, 1);
         assertEquals(result, 30);
     }
-    
+
     @Test
     //tests p1, a=F  -> P2,P3 reachable
     //tests p2, bcd=FTT
@@ -57,23 +67,23 @@ public class CalTest {
         int result = Cal.cal(1, 1, 12, 31, 100);
         assertEquals(result, 364);
     }
-    
+
     @Test
     //tests p1, a=F  -> P2,P3 reachable
-    //tests p2, bcd=FFT 
-    //tests p3, 
+    //tests p2, bcd=FFT
+    //tests p3,
     public void test_P2cT_P2FFT(){
         int result = Cal.cal(1, 1, 12, 31, 4);
         assertEquals(result, 365);
     }
-    
+
     @Test
     //tests p1, a=f -> P2,P3 reachable
     //tests p2, bcd=FTF
-    //tests p3, 
+    //tests p3,
     public void test_P2FTF(){
         int result = Cal.cal(1, 1, 12, 31, 400);
         assertEquals(result, 365);
     }
-    
+
 }
